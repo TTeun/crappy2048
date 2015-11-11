995a4df4502f970b7caee6803b0b5653e8f859e6
TTeun <teunverstraaten@gmail.com>
2015-11-11 15:49:37 +0100

    onzin

--- ai_example.js
+++ ai_example.js
@@ -23,7 +23,7 @@ function Ai() {
         //              This changes the state of the grid object, so you should probably copy() the grid before using this.
         //              Naturally the modified state doesn't contain information about new tiles.
         //              Method returns true if you can move to that direction, false otherwise.
-        var num_free = 0;
+        var free = 0;
         // for (i = 0; i < 4; i++){
         //     for (j = 0; j < 4; j++){
         //         if (grid.cells[i][j] == null){
